75866178ab3477041cb1c644eed7c23eda3ffeb0
Miguel Dönicke <miguel@doenicke.io>
2021-10-22 19:44:17 +0200

    add iOS project

---
+++ CleanApp/iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS
+//
+//  Created by Miguel Dönicke on 22.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
